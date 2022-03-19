8951d176e3f8c77fdb42909748327a3f87bcb03b
raisiec <75871695+raisiec@users.noreply.github.com>
2022-03-19 12:39:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,34 @@
-#2022-2-5 Wordle Recreation (No GUI)
-#Authors: Kingsley & Raisie
-#Coding language : Python 3
+# Authors: Kingsley + Raisie
+# Date: 19/3/2022
+# Topic: Wordle text-based lesson
 
-#Import Stuff
 import random
-#from tkinter import Tk, Canvas
 
-#List of 5 Letter Words
-global words
-words = ["aloft", "heart", "queen", "those", "learn"]
+words = ["there", "queen", "aloft", "dream", "raise"]
 
-#Make the variables needed
-global lives
-lives = 6
-answer = 0
-
-#Define the functions need
+# Create a function for choosing the random word
 def pickRandomWord():
-  #First, pick a number as 'num'
-  num = random.randint(0, 4)
-
-  #Then, in the 'words' array, find 'num' and put it in 'answer'
   global answer
-  answer = words[num]
+  answer = words[random.randint(0, len(words) - 1)]
 
-def checkHint(guess, answer):
-  for i in range(5):
-    if (answer[i].lower() == guess.lower()):
-      return True
-  return False
-
-def checkWord():
-  hint_list = ''
-  for i in range(5):
-    if (guess[i].lower() == answer[i].lower()):
-      hint = "*"
-    elif checkHint(guess[i], answer):
-      hint = "o"
-    else:
-      hint = "?"
-    hint_list = hint_list + hint
-  print(hint_list)
-
+  print(answer)
+  
 pickRandomWord()
 
-#Main Loop
-while (lives > 0):
-  print("Lives left: ", lives)
-  print("Your hints are (* = Right Letter and Right Place, o = Right Letter but Wrong Place, ? = nothing.")
-  guess = input("Guess the five-letter word!")  
+lives = 3
 
-  checkWord()
+for i in range(lives):
+  print(lives)
+  guess = input("Guess the five-letter word.")
 
   if guess == answer:
-    print("You win!")
-    break 
+    print("Yes.")
+    break
   else:
-    print("You Lose, try again!")
-    lives -= 1
+    print("No.")
 
-#End of code
+  lives = lives - 1
+
+print("The correct answer is: " + answer)
+
+# End
